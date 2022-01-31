5f79dd397a053957554c558cd87039185a08facd
Daehwan Nam <dhnam@postech.ac.kr>
2022-02-01 07:18:42 +0900

    binding update

--- init.lisp
+++ init.lisp
@@ -1,9 +1,4 @@
 
-;; (progn
-;;   ;; allow multiple instances of Nyxt
-;;   ;; https://discourse.atlas.engineer/t/my-lightweight-configuration/47
-;;   (setf *socket-path* nil))
-
 (defmacro comment (&rest args)
   nil)
 
@@ -15,57 +10,17 @@
   (define-configuration browser
       ((remote-execution-p :t)
        ;; (session-restore-prompt :always-restore)
-       (session-restore-prompt :never-restore)
-       )))
+       (session-restore-prompt :never-restore))))
+
+(comment
+ ;; config examples
+ ;; - https://discourse.atlas.engineer/t/my-lightweight-configuration/47
+ )
 
 (comment
  ;; to make new nyxt window from command line
  ;; $ nyxt --remote --eval '(make-window)'
  (define-configuration browser ((remote-execution-p :t))))
-
-(comment
- (define-configuration nyxt/web-mode:web-mode
-     ;; QWERTY home row.
-     ((nyxt/web-mode:hints-alphabet "ASDFGHJKL"))))
-
-(comment
- (define-configuration web-buffer
-     ;; https://discourse.atlas.engineer/t/my-lightweight-configuration/47
-     ((default-modes (append '(auto-mode emacs-mode) %slot-default%)))))
-
-(comment
- (define-configuration web-buffer
-     ;; https://discourse.atlas.engineer/t/my-lightweight-configuration/47
-     ((default-modes (append '(auto-mode emacs-mode) %slot-default%))
-      (override-map (let ((map (make-keymap "my-web-buffer-override-map")))
-                      (define-key map
-                        "C-4" 'quit
-                        "C-8" 'make-window
-                        "C-0" 'delete-current-window
-                        "M-n" 'nyxt/web-mode:scroll-down
-                        "M-p" 'nyxt/web-mode:scroll-up
-                        )
-                      map)))))
-
-(comment
- (define-command-global scroll-small-page-down ()
-   "Scroll down by a small height."
-   (pflet ((scroll-page-down ()
-                             (ps:chain window (scroll-by 0 30))))
-          (scroll-page-down)))
-
- (define-command-global scroll-small-page-up ()
-   "Scroll up by a small height."
-   (pflet ((scroll-page-up ()
-                           (ps:chain window (scroll-by 0 -30))))
-          (scroll-page-up)))
-
- (define-configuration buffer
-     ((default-modes (append '(nyxt::emacs-mode) %slot-default%))
-      (override-map (let ((map (make-keymap "my-override-map")))
-                      (define-key map "M-n" 'scroll-small-page-down)
-                      (define-key map "M-p" 'scroll-small-page-up)
-                      map)))))
 
 (progn
   (defvar *my-search-engines*
@@ -88,30 +43,12 @@
 (define-configuration buffer
     ;; https://discourse.atlas.engineer/t/how-to-make-my-key-bindings-work-on-the-prompt-buffer-too/206/4
     ((default-modes (append '(emacs-mode) %slot-default%))
-     ;; (nyxt/web-mode:hints-alphabet "ASDFGHJKL")
+
      (override-map (let ((map (make-keymap "my-buffer-override-map")))
                      (define-key map
-                       "C-4" 'quit
-                       "C-8" 'make-window
-                       "C-0" 'delete-current-window
-
-                       ;; "M-x" 'execute-command
-                       ;; "C-n" 'nyxt/web-mode:scroll-down
-                       ;; "C-p" 'nyxt/web-mode:scroll-up
-                       "C-f" 'nyxt/web-mode:scroll-right
-                       "C-b" 'nyxt/web-mode:scroll-left
-                       ;; "C-g" 'query-selection-in-search-engine
-                       ;; "M-s->" 'nyxt/web-mode:scroll-to-bottom
-                       ;; "M-s-<" 'nyxt/web-mode:scroll-to-top
-
-                       "M-f" 'nyxt/input-edit-mode:cursor-forwards-word
-                       "M-b" 'nyxt/input-edit-mode:cursor-backwards-word
-                       "C-e" 'nyxt/input-edit-mode:cursor-forwards-word
-                       "C-a" 'nyxt/input-edit-mode:cursor-backwards-word
-                       "M-backspace" 'nyxt/input-edit-mode:delete-backwards-word
-                       "M-d" 'nyxt/input-edit-mode:delete-forwards-word
-
-                       "M-s-D" 'list-downloads ; M-D
+                       "C-x C-c" 'quit
+                       "C-q 8" 'make-window
+                       "C-q 0" 'delete-current-window
 
                        "C-q" 'nothing
                        "C-q b" 'switch-buffer
@@ -119,19 +56,15 @@
                        "C-q C-k" 'delete-current-buffer
                        "C-q r" 'reopen-buffer
                        "C-q C-r" 'reopen-last-buffer
+
                        "C-q C-d" 'download-open-file
+                       "C-q d" 'list-downloads       ; M-D
+                       ;; "M-s-D" 'list-downloads       ; M-D
 
-                       ;; "M-n" 'nyxt/web-mode:scroll-down
-                       ;; "M-p" 'nyxt/web-mode:scroll-up
-
-                       "C-s" 'nyxt/web-mode:search-buffer
-                       "M-j" 'nyxt/web-mode:follow-hint-new-buffer-focus
-                       "M-r" 'nyxt/visual-mode:visual-mode
-                       "M-e" 'nyxt/input-edit-mode:input-edit-mode
                        "C-M-t" 'nyxt/passthrough-mode:passthrough-mode
                        )))
 
-     ;; the last search engine becomes the default
+      ;; the last search engine becomes the default
      (search-engines (append %slot-default%
                              (mapcar (lambda (engine) (apply 'make-search-engine engine))
                                      (append *my-search-engines*
@@ -142,43 +75,63 @@
      (bookmarks-path (make-instance 'bookmarks-data-path
                                     :basename "~/script/common/data/nyxt/bookmarks.lisp"))))
 
+(define-configuration nyxt/web-mode:web-mode
+    ;; https://discourse.atlas.engineer/t/how-to-make-my-key-bindings-work-on-the-prompt-buffer-too/206/4
+    ((default-modes (append '(emacs-mode) %slot-default%))
+      ;; (nyxt/web-mode:hints-alphabet "ASDFGHJKL")
+     (keymap-scheme
+      (define-scheme
+          (:name-prefix "my-base" :import %slot-default%)
+        scheme:emacs
+        (list
+         "M-9" 'switch-buffer-previous
+         "M-0" 'switch-buffer-next
+         "C-9" 'nyxt/web-mode:history-backwards
+         "C-0" 'nyxt/web-mode:history-forwards
+
+         ;; "M-x" 'execute-command
+         ;; "C-n" 'nyxt/web-mode:scroll-down
+         ;; "C-p" 'nyxt/web-mode:scroll-up
+
+         "C-f" 'nyxt/web-mode:scroll-right
+         "C-b" 'nyxt/web-mode:scroll-left
+
+         ;; "C-g" 'query-selection-in-search-engine
+         ;; "M-s->" 'nyxt/web-mode:scroll-to-bottom
+         ;; "M-s-<" 'nyxt/web-mode:scroll-to-top
+
+         "M-f" 'nyxt/input-edit-mode:cursor-forwards-word
+         "M-b" 'nyxt/input-edit-mode:cursor-backwards-word
+         "C-e" 'nyxt/input-edit-mode:cursor-forwards-word
+         "C-a" 'nyxt/input-edit-mode:cursor-backwards-word
+         "M-backspace" 'nyxt/input-edit-mode:delete-backwards-word
+         "M-d" 'nyxt/input-edit-mode:delete-forwards-word
+         "C-q C-w" 'nyxt/web-mode:copy-hint-url
+
+         ;; "M-n" 'nyxt/web-mode:scroll-down
+         ;; "M-p" 'nyxt/web-mode:scroll-up
+
+         "C-s" 'nyxt/web-mode:search-buffer
+         "M-j" 'nyxt/web-mode:follow-hint-new-buffer-focus
+         "M-r" 'nyxt/visual-mode:visual-mode
+         "M-e" 'nyxt/input-edit-mode:input-edit-mode
+         )))))
+
 (define-configuration nyxt/passthrough-mode:passthrough-mode
     ((keymap-scheme
-      (define-scheme "application"
+      (define-scheFme "application"
         scheme:cua
         (list
          "C-z" 'nothing
          )))))
 
-(comment
- (define-command-prompt delete-forwards (prompt-buffer)
-   "Move cursor forward by one character."
-   (with-current-buffer prompt-buffer
-     (nyxt/input-edit-mode:delete-forwards)))
-
- (define-command-prompt delete-forwards-word (prompt-buffer)
-   "Move cursor forward by one character."
-   (with-current-buffer prompt-buffer
-     (nyxt/input-edit-mode:delete-forwards-word)))
-
- (define-command-prompt delete-backwards-word (prompt-buffer)
-   "Move cursor backward by one character."
-   (with-current-buffer prompt-buffer
-     (nyxt/input-edit-mode:delete-backwards-word))))
-
 (define-configuration (prompt-buffer)
     ((default-modes (append '(emacs-mode) %slot-default%))
-     (hide-suggestion-count-p t)
+     ;; (hide-suggestion-count-p t)
      (override-map (let ((map (make-keymap "my-prompt-buffer-override-map")))
                      (define-key map
-                       "M-N" 'nyxt/prompt-buffer-mode:scroll-other-buffer-down
-                       "M-P" 'nyxt/prompt-buffer-mode:scroll-other-buffer-up
+                       ;; "C-M-n" 'nyxt/prompt-buffer-mode:scroll-other-buffer-down
+                       ;; "C-M-p" 'nyxt/prompt-buffer-mode:scroll-other-buffer-up
                        "M-n" 'nyxt/prompt-buffer-mode:scroll-other-buffer-down
                        "M-p" 'nyxt/prompt-buffer-mode:scroll-other-buffer-up
-
-                       ;; "C-d" 'delete-forwards
-                       ;; "M-d" 'delete-forwards-word
-                       ;; "M-backspace" 'delete-backwards-word
-
-                       ;; "C-s" 'nyxt/prompt-buffer-mode:copy-selection
                        )))))
